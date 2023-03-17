ccf67688c06c1ccc779beec87104b4d60de2a6c2
jeanlindooooo <127588891+jeanlindooooo@users.noreply.github.com>
2023-03-17 11:46:49 -0300

    Delete discord.js

--- utils/discord.js
+++
@@ -1,5 +0,0 @@
-const rpc = require('discordrpcgenerator');
-
-const getImage = async(applicationID, imageKey) => await rpc.getRpcImage(applicationID, imageKey) 
-
-module.exports.getImage = getImage
